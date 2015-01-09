a4ff4c7ac8ee4c82a8f6026c5985ae1572e0dcb4
David Zulaica <zulaica@gmail.com>
2015-01-09 10:07:41 -0800

    Fixes indentation.

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -4,8 +4,8 @@ require('word_counter')
 describe('Array#word_counter') do
 
   it("compares two identical strings and returns a value of 1 because they match.") do
-      expect((["a"]).word_counter("a")).to(eq(1))
-      expect((["pizza"]).word_counter("pizza")).to(eq(1))
+    expect((["a"]).word_counter("a")).to(eq(1))
+    expect((["pizza"]).word_counter("pizza")).to(eq(1))
   end
 
 end
