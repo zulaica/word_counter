027b807fecdffcc380d548d9fee1fde18e71bee9
David Zulaica <zulaica@gmail.com>
2015-01-09 11:24:44 -0800

    Ignore leter case when comparing words and increase total accordingly.

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -2,7 +2,7 @@ class String
   define_method(:word_counter) do |word_to_count|
 
     total = 0
-    words_to_check = split()
+    words_to_check = downcase().split()
 
     words_to_check.each() do |current_word|
       if current_word.include? word_to_count
