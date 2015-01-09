9f7d73791e8ad2ee7ed847b216256446d7707d00
David Zulaica <zulaica@gmail.com>
2015-01-09 14:33:56 -0800

    Add instance variables and logic to app.rb

--- app.rb
+++ app.rb
@@ -8,5 +8,19 @@ get('/') do
 end
 
 get('/results') do
+  word_to_count = params.fetch('word_to_count')
+  words_to_check = params.fetch('words_to_check')
+  number_of_times = params.fetch('words_to_check').word_counter(params.fetch('word_to_count'))
+  preposition = "times"
+
+  if number_of_times.==(1)
+    preposition = "time"
+  end
+
+  @word_to_count = word_to_count
+  @words_to_check = words_to_check
+  @number_of_times = number_of_times
+  @preposition = preposition
+
   erb(:results)
 end
