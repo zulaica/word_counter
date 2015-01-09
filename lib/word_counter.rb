3106bc3302472f1a451ba4afa0ccb2aace8d50db
David Zulaica <zulaica@gmail.com>
2015-01-09 10:50:17 -0800

    Return a value of 1 if the word we are looking for appears in the string at least once.

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,5 +1,13 @@
-class Array
+class String
   define_method(:word_counter) do |word_to_count|
+    total = 0
+    words_to_check = split()
+
+    if words_to_check.include? word_to_count
+      total = total.+(1)
+    end
+
+    total
 
   end
 end
