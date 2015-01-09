df2077716f9265cde286539bbd3ef7593cee98dc
David Zulaica <zulaica@gmail.com>
2015-01-09 10:40:26 -0800

    Change our class to String as the method will later split that String into an Array.

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -1,11 +1,11 @@
 require('rspec')
 require('word_counter')
 
-describe('Array#word_counter') do
+describe('String#word_counter') do
 
   it("compares two identical strings and returns a value of 1 because they match.") do
-    expect((["a"]).word_counter("a")).to(eq(1))
-    expect((["pizza"]).word_counter("pizza")).to(eq(1))
+    expect(("a").word_counter("a")).to(eq(1))
+    expect(("pizza").word_counter("pizza")).to(eq(1))
   end
 
 end
