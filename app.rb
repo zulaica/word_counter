c5912bb89c7d88788cbb762d5e69d18ce56a9fb8
David Zulaica <zulaica@gmail.com>
2015-01-09 11:55:27 -0800

    Add views to app.rb.

--- app.rb
+++ app.rb
@@ -2,3 +2,11 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word_counter')
+
+get('/') do
+  erb(:form)
+end
+
+get('/results') do
+  erb(:results)
+end
