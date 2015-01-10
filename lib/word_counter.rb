9a3c622d2d3709ecd5d0ab7a0cc2ff12d3a76f51
David Zulaica <zulaica@gmail.com>
2015-01-09 16:46:19 -0800

    Fix indentation.

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -6,7 +6,7 @@ class String
 
     words_to_check.each() do |current_word|
       if current_word.length().eql?(word_to_count.length()) && current_word.include?(word_to_count.downcase())
-          total = total.+(1)
+        total = total.+(1)
       end
     end
 
